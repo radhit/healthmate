d61d13a2386099ec3e5164cf2f3f2f3698145a53
r_adhita <riska.adhita079@gmail.com>
2020-09-18 13:19:48 +0700

    feat: add linechart on rapor view

--- app/src/main/java/com/healthmate/menu/mom/rapor/view/GrafikBbFragment.kt
+++ app/src/main/java/com/healthmate/menu/mom/rapor/view/GrafikBbFragment.kt
@@ -1,5 +1,6 @@
 package com.healthmate.menu.mom.rapor.view
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProviders
@@ -11,6 +12,11 @@ import com.healthmate.common.constant.Urls
 import com.healthmate.common.functions.Fun
 import com.healthmate.di.injector
 import androidx.lifecycle.Observer
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.healthmate.commons.utils.AxisDateFormatter
 import com.healthmate.menu.mom.rapor.data.AncModel
 import com.healthmate.menu.reusable.data.MasterViewModel
 import kotlinx.android.synthetic.main.fragment_grafik_bb.*
@@ -23,6 +29,7 @@ class GrafikBbFragment : BaseFragment() {
         ViewModelProviders.of(this, injector.masterVM()).get(MasterViewModel::class.java)
     }
     var dataAnc: AncModel = AncModel()
+    var dataAncs: ArrayList<AncModel> = ArrayList()
 
     companion object {
         fun newInstance(): GrafikBbFragment = GrafikBbFragment()
@@ -46,7 +53,9 @@ class GrafikBbFragment : BaseFragment() {
                             if (result.data!!.size>0){
                                 tv_loading.visibility = View.GONE
                                 ll_data.visibility = View.VISIBLE
+                                dataAncs.addAll(result.data!!)
                                 dataAnc = result.data!![0]
+                                setData()
                             } else{
                                 tv_loading.visibility = View.VISIBLE
                                 ll_data.visibility = View.GONE
@@ -61,5 +70,41 @@ class GrafikBbFragment : BaseFragment() {
                         }
                     }
                 })
+    }
+
+    private fun setData() {
+        tv_bb_awal.text = "${dataAnc.initial_weight} Kg"
+        tv_minggu_ke.text = dataAnc.week_of_pregnancy
+        tv_bb_now.text = "${dataAnc.weight} Kg"
+        tv_perubahan_bb.text = "${dataAnc.differenced_weight} Kg"
+        tv_suggested.text = "Suggested Weight\n${dataAnc.suggested_weight} Kg"
+        setGrafik()
+    }
+
+    private fun setGrafik() {
+        val data = ArrayList<Entry>()
+        var floatNumber: String
+        var floatValue: String
+        var date = ArrayList<String>()
+        for (position in 0..dataAncs.size-1){
+            floatNumber = "${position}F"
+            floatValue = "${dataAncs[position].weight}F"
+            data.add(Entry(floatNumber.toFloat(),floatValue.toFloat()))
+            date.add(dataAncs[position].created_time.split("T")[0])
+        }
+
+        val lineBB = LineDataSet(data, "Berat Badan")
+        lineBB.mode = LineDataSet.Mode.CUBIC_BEZIER
+        lineBB.color = resources.getColor(R.color.colorPrimary)
+        lineBB.circleRadius = 5f
+        lineBB.setCircleColor(resources.getColor(R.color.colorPrimary))
+
+        lineChart.legend.isEnabled = false
+        lineChart.description.isEnabled = false
+        lineChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+        lineChart.data = LineData(lineBB)
+        lineChart.animateXY(100, 500)
+        val tanggal = AxisDateFormatter(date.toArray(arrayOfNulls<String>(date.size)))
+        lineChart.xAxis?.setValueFormatter(tanggal)
     }
 }
